22a9e023ee649cd356282e7375c8aa3932d6b7e5
Kalvarez21g <108185745+Kalvarez21g@users.noreply.github.com>
2022-10-17 13:27:56 +0200

    Add files via upload
    Practica completa

---
+++ p-var-Kalvarez21g/README.md
@@ -0,0 +1,25 @@
+[![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-c66648af7eb3fe8bc4f294546bfd86ef473780cde1dea487d3c4ff354943c9ae.svg)](https://classroom.github.com/online_ide?assignment_repo_id=8707680&assignment_repo_type=AssignmentRepo)
+# Manejo de Variables
+
+El objetivo principal de la práctica es poner en uso los conocimientos sobre variables adquiridos
+en clase de teoría. Para ello se deberán resolver en lenguaje JAVA una serie de ejercicios sobre
+tipos de datos básicos, operadores, conversiones de tipo y cadenas de caracteres.
+
+Junto al objetivo general, la práctica tiene una serie de objetivos específicos. A saber:
+
+- Aprender a resolver errores de compilación.
+- Conocer la forma adecuada de mostrar el contenido de una clase por pantalla.
+
+## Variables 
+Una variable se entiende como un espacio en memoria dónde se guarda una información. El
+formato, tamaño y manejo de dicha variable depende del tipo asociado a la variable. El lenguaje
+Java proporciona variables de dos tipos:
+
+- Tipo básico. Permite almacenar constantes y literales que representan valores
+específicos.
+- Tipo no básico. Permiten almacenar referencias a objetos que representan entidades.
+
+Junto a su concepción teórica, es necesario poner en práctica los conocimientos teóricos
+adquiridos a fin de garantizar la correcta utilización de las variables. Basándose en esta premisa,
+la presente práctica propone la realización de una serie de ejercicios sobre el manejo de
+variables.
---
+++ p-var-Kalvarez21g/src/es/unileon/prg/tema5/Apartado030103.java
@@ -0,0 +1,92 @@
+package es.unileon.prg.tema5;
+
+/**
+ * Clase con los ejercicios correspondientes a operadores matematicos. La clase
+ * "Math"
+ *
+ * @author PRG
+ * @version 1.0
+ */
+public class Apartado030103 extends Apartado {
+
+	protected String obtenerPractica() {
+		return "P-VAR";
+	}
+
+	protected String obtenerBloque() {
+		return "Operadores matematicos - Clase <<Math>>";
+	}
+
+	/**
+	 * Operadores matematicos - Clase <<Math>> - Ejercicio1.
+	 *
+	 * </br>
+	 *
+	 * Consultar la clase <<Math>> del API de Java y programar el codigo
+	 * necesario para realizar la operacion: obtener la raiz cuadrada de 256
+	 */
+	public void ejercicio01() {
+		cabecera("01", "Calcular la raiz cuadrada de un numero");
+
+		// Inicio modificacion
+		double raizCuadrada= Math.sqrt(256);
+
+		// Fin modificacion
+	}
+
+	/**
+	 * Operadores matematicos - Clase <<Math>> - Ejercicio2.
+	 *
+	 * </br>
+	 *
+	 * Consultar la clase <<Math>> del API de Java y programar el codigo
+	 * necesario para realizar la operacion: obtener el resultado de elevar al
+	 * cubo el numero 9
+	 */
+	public void ejercicio02() {
+		cabecera("02", "Calcular potencias");
+
+		// Inicio modificacion
+		int num=9;
+		double cubo;
+		cubo = Math.pow( num, 3);
+		System.out.println(cubo);
+		// Fin modificacion
+	}
+
+	/**
+	 * Operadores matematicos - Clase <<Math>> - Ejercicio3.
+	 *
+	 * </br>
+	 *
+	 * Consultar la clase <<Math>> del API de Java y programar el codigo
+	 * necesario para generar un numero aleatorio contenido entre 5 y 10
+	 */
+	public void ejercicio03() {
+		cabecera("03", "Generar numeros aleatorios");
+
+		// Inicio modificacion
+		int num;
+
+		num= (int) ((Math.random()*10)+5);
+		System.out.println(num);
+		// Fin modificacion
+	}
+
+	/**
+	 * Operadores matematicos - Clase <<Math>> - Ejercicio4.
+	 *
+	 * </br>
+	 *
+	 * Consultar la clase <<Math>> del API de Java y programar el codigo necesario
+	 * para conocer la superficie de un circulo de diez unidades de radio.
+	 */
+	public void ejercicio04() {
+		cabecera("04", "Calcular la superficie de un circulo");
+
+		double superficie= Math.PI * Math.pow(10,2);
+		System.out.println(superficie);
+		// Inicio modificacion
+		// Fin modificacion
+	}
+}
